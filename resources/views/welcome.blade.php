7a69bb4170a038db85302ae79d9a434ff789cb65
yessentay01 <ye.kurmanbay@iitu.edu.kz>
2023-04-24 00:36:30 +0600

    remove about us

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,92 +83,89 @@
 
 <main id="main">
 
-    <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-        <div class="container" data-aos="fade-up">
+{{--    <!-- ======= About Section ======= -->--}}
+{{--    <section id="about" class="about">--}}
+{{--        <div class="container" data-aos="fade-up">--}}
 
-            <div class="section-header">
-                <h2>About Us</h2>
-                <p>Learn More <span>About Us</span></p>
-            </div>
+{{--            <div class="section-header">--}}
+{{--                <h2>About Us</h2>--}}
+{{--                <p>Learn More <span>About Us</span></p>--}}
+{{--            </div>--}}
 
-            <div class="row gy-4">
-                <div class="col-lg-7 position-relative about-img"
-                     style="background-image: url('{{asset('assets/img/iitu-partners.jpg')}}') ;" data-aos="fade-up"
-                     data-aos-delay="150">
-                </div>
-                <div class="col-lg-5 d-flex align-items-start" data-aos="fade-up" data-aos-delay="300">
-                    <div class="content ps-0 ps-lg-5">
-                        <p class="fst-italic">
-                            Welcome, dear teacher, student! We are glad to see you here again, so what would you like to order today ?
+{{--            <div class="row gy-4">--}}
+{{--                <div class="col-lg-7 position-relative about-img"--}}
+{{--                     style="background-image: url('{{asset('assets/img/iitu-partners.jpg')}}') ;" data-aos="fade-up"--}}
+{{--                     data-aos-delay="150">--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-5 d-flex align-items-start" data-aos="fade-up" data-aos-delay="300">--}}
+{{--                    <div class="content ps-0 ps-lg-5">--}}
+{{--                        <p class="fst-italic">--}}
+{{--                            Welcome, dear teacher, student! We are glad to see you here again, so what would you like to order today ?--}}
 
-                            <br>
-                            Quickly go to the menu and see what's on today. We are waiting for you and your application with impatience! It tastes better with us!
-                        </p>
-                    </div>
-                </div>
-            </div>
+{{--                            <br>--}}
+{{--                            Quickly go to the menu and see what's on today. We are waiting for you and your application with impatience! It tastes better with us!--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
 
-        </div>
-    </section><!-- End About Section -->
+{{--        </div>--}}
+{{--    </section><!-- End About Section -->--}}
 
 
-    <section id="why-us" class="why-us section-bg">
-        <div class="container" data-aos="fade-up">
+{{--    <section id="why-us" class="why-us section-bg">--}}
+{{--        <div class="container" data-aos="fade-up">--}}
 
-            <div class="row gy-4">
+{{--            <div class="row gy-4">--}}
 
-                <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
-                    <div class="why-box">
-                        <h3>Why Choose IITU Canteen?</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
-                            Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus
-                            optio ad corporis.
-                        </p>
-                        <div class="text-center">
-                            <a href="#" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
-                        </div>
-                    </div>
-                </div><!-- End Why Box -->
+{{--                <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">--}}
+{{--                    <div class="why-box">--}}
+{{--                        <h3>Events</h3>--}}
+{{--                        <p>--}}
+{{--                            Plan and book a time for your event at the university so you don't have to spend time waiting in line or waiting for a table to become available.--}}
+{{--                        </p>--}}
+{{--                        <div class="text-center">--}}
+{{--                            <a href="#" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div><!-- End Why Box -->--}}
 
-                <div class="col-lg-8 d-flex align-items-center">
-                    <div class="row gy-4">
+{{--                <div class="col-lg-8 d-flex align-items-center">--}}
+{{--                    <div class="row gy-4">--}}
 
-                        <div class="col-xl-4" data-aos="fade-up" data-aos-delay="200">
-                            <div class="icon-box d-flex flex-column justify-content-center align-items-center">
-                                <i class="bi bi-clipboard-data"></i>
-                                <h4>Corporis voluptates officia eiusmod</h4>
-                                <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut
-                                    aliquip</p>
-                            </div>
-                        </div><!-- End Icon Box -->
+{{--                        <div class="col-xl-4" data-aos="fade-up" data-aos-delay="200">--}}
+{{--                            <div class="icon-box d-flex flex-column justify-content-center align-items-center">--}}
+{{--                                <i class="bi bi-clipboard-data"></i>--}}
+{{--                                <h4>Corporis voluptates officia eiusmod</h4>--}}
+{{--                                <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut--}}
+{{--                                    aliquip</p>--}}
+{{--                            </div>--}}
+{{--                        </div><!-- End Icon Box -->--}}
 
-                        <div class="col-xl-4" data-aos="fade-up" data-aos-delay="300">
-                            <div class="icon-box d-flex flex-column justify-content-center align-items-center">
-                                <i class="bi bi-gem"></i>
-                                <h4>Ullamco laboris ladore pan</h4>
-                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt</p>
-                            </div>
-                        </div><!-- End Icon Box -->
+{{--                        <div class="col-xl-4" data-aos="fade-up" data-aos-delay="300">--}}
+{{--                            <div class="icon-box d-flex flex-column justify-content-center align-items-center">--}}
+{{--                                <i class="bi bi-gem"></i>--}}
+{{--                                <h4>Ullamco laboris ladore pan</h4>--}}
+{{--                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia--}}
+{{--                                    deserunt</p>--}}
+{{--                            </div>--}}
+{{--                        </div><!-- End Icon Box -->--}}
 
-                        <div class="col-xl-4" data-aos="fade-up" data-aos-delay="400">
-                            <div class="icon-box d-flex flex-column justify-content-center align-items-center">
-                                <i class="bi bi-inboxes"></i>
-                                <h4>Labore consequatur incidid dolore</h4>
-                                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
-                            </div>
-                        </div><!-- End Icon Box -->
+{{--                        <div class="col-xl-4" data-aos="fade-up" data-aos-delay="400">--}}
+{{--                            <div class="icon-box d-flex flex-column justify-content-center align-items-center">--}}
+{{--                                <i class="bi bi-inboxes"></i>--}}
+{{--                                <h4>Labore consequatur incidid dolore</h4>--}}
+{{--                                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>--}}
+{{--                            </div>--}}
+{{--                        </div><!-- End Icon Box -->--}}
 
-                    </div>
-                </div>
+{{--                    </div>--}}
+{{--                </div>--}}
 
-            </div>
+{{--            </div>--}}
 
-        </div>
-    </section>
+{{--        </div>--}}
+{{--    </section>--}}
 
     <!-- ======= Stats Counter Section ======= -->
     <section id="stats-counter" class="stats-counter">
